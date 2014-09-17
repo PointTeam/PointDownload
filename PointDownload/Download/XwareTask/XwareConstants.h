6db4be16e20344ad04e1e54cd4a1abf8ffefcea4
Choldrim <724447804@qq.com>
2014-09-17 10:37:35 +0800

    change the debug info

--- PointDownload/Download/XwareTask/XwareConstants.h
+++ PointDownload/Download/XwareTask/XwareConstants.h
@@ -30,8 +30,8 @@
 static struct XwareConstants
 {
     // is output the debug info
-    static const bool DEBUG = true;
-    static const bool DEBUG_2 = true;
+    static const bool DEBUG = false;
+    static const bool DEBUG_2 = false;
 
     int  ETM_PORT;  // default port
 
